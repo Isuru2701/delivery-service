162838fba6fa2943d917c6ed4194d542e8b25607
Duleesha000 <145181627+Duleesha000@users.noreply.github.com>
2023-10-20 19:33:43 +0530

    edit

--- src/main/java/com/devtechnexus/delivery/controller/DeliveryController.java
+++ src/main/java/com/devtechnexus/delivery/controller/DeliveryController.java
@@ -1,10 +1,41 @@
 package com.devtechnexus.delivery.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.devtechnexus.delivery.model.Delivery;
+import com.devtechnexus.delivery.service.DeliveryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 @RestController
-@RequestMapping(path = "/deliveries")
+@RequestMapping("/deliveries")
 public class DeliveryController {
+
+    @Autowired
+    private DeliveryService service;
+
+    @GetMapping
+    public List<Delivery> getAll() {
+        return service.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Delivery getDeliveryByID(@PathVariable int id) {
+        return service.getDeliveryByID(id);
+    }
+
+    @PostMapping
+    public Delivery createDelivery(@RequestBody Delivery delivery) {
+        return service.createDelivery(delivery);
+    }
+
+    @PutMapping
+    public Delivery updateDelivery(@RequestBody Delivery delivery) {
+        return service.updateDelivery(delivery);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteDelivery(@PathVariable int id) {
+        service.deleteDelivery(id);
+    }
 }
--- src/main/java/com/devtechnexus/delivery/service/DeliveryService.java
+++ src/main/java/com/devtechnexus/delivery/service/DeliveryService.java
@@ -1,5 +1,38 @@
 package com.devtechnexus.delivery.service;
 
+import com.devtechnexus.delivery.model.Delivery;
+import com.devtechnexus.delivery.repository.DeliveryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class DeliveryService {
+
+    @Autowired
+    private DeliveryRepository repository;
+
+    public List<Delivery> getAll() {
+        return repository.findAll();
+    }
+
+    public Delivery getDeliveryByID(int id) {
+        Optional<Delivery> delivery = repository.findById(id);
+        return delivery.orElse(null);
+    }
+
+    public Delivery createDelivery(Delivery delivery) {
+        return repository.save(delivery);
+    }
+
+    public Delivery updateDelivery(Delivery delivery) {
+        return repository.save(delivery);
+    }
+
+    public void deleteDelivery(int id) {
+        repository.deleteById(id);
+    }
 }
+
